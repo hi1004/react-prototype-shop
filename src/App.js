5ec43d447a87c6345c0946fa51623848806b859c
OhByeongHeon <hi1004@github.com>
2022-06-14 21:57:48 +0900

    start project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Header from './components/Header';
+import Prototypes from './components/Prototypes';
+import Orders from './components/Orders';
+import Footer from './components/Footer';
+import './styles/css/App.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <Prototypes />
+        <Orders />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
